a915d73d188d286358ba474efdb9730414031506
SongoMen <pat.kozlowski2000@onet.pl>
2020-02-12 18:44:00 +0100

    translations, firebase, deploy

--- src/components/elements/Cookies.js
+++ src/components/elements/Cookies.js
@@ -1,34 +1,33 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import Cookies from "universal-cookie";
 
-import { ReactComponent as CookieIcon } from "../../icons/cookie.svg";
-
+import {ReactComponent as CookieIcon} from "../../icons/cookie.svg";
 
 const CookiesNotification = () => {
-	let render = true
-	const cookies = new Cookies();
-	const [show, setShow] = useState(true);
-	function save() {
-		cookies.set("cookies", true, {
-			path: "/",
-			expires: new Date(Date.now() + 1000000000)
-		});
-		render = false;
-		setShow(false);
-	}
+  let render = true;
+  const cookies = new Cookies();
+  const [show, setShow] = useState(true);
+  function save() {
+    cookies.set("cookies", true, {
+      path: "/",
+      expires: new Date(Date.now() + 1000000000),
+    });
+    render = false;
+    setShow(false);
+  }
 
-	return (
-		!cookies.get("cookies") &&
-		show && (
-			<div className="Cookies">
-			<CookieIcon />
-			<h4>This site is using cookies.</h4>
-			<button className="btn" onClick={() => save()}>
-			Akceptuję
-			</button>
-			</div>
-		)
-	);
+  return (
+    !cookies.get("cookies") &&
+    show && (
+      <div className="Cookies">
+        <CookieIcon />
+        <h4>This site is using cookies.</h4>
+        <button className="btn" onClick={() => save()}>
+          Accept
+        </button>
+      </div>
+    )
+  );
 };
 
 export default CookiesNotification;
